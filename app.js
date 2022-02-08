0cf5c4d6d723f34acb5d3a6c0d5d0a051c74d5b7
santhosh312 <sanet3136@gmail.com>
2022-02-08 14:51:01 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const path = require("path");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const app = express();
+
+const dbPath = path.join(__dirname, "twitterClone.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+app.get("/", async (req, res) => {
+  const data = await db.all(`SELECT * FROM user;`);
+  res.send(data);
+});
+
+initializeDBAndServer();
+
+module.exports = app;
